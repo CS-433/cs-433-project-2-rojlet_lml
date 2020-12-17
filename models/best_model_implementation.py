45d996d5b24a0cba882ab592b8278790381f9f5c
Ahmed Ben Haj Yahia <ahmed.benhajyahia@epfl.ch>
2020-12-17 09:40:27 +0100

    update run.py

--- models/best_model_implementation.py
+++ models/best_model_implementation.py
@@ -45,8 +45,45 @@ def run_SVM(X_test):
 
 
 def build_model():
-    return 0
+    import tensorflow as tf
+    from transformers import TFBertForSequenceClassification
+    model= TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE, from_logits=True)
+    opt = tf.keras.optimizers.Adam(learning_rate=3e-5)
 
-def best_model(X_test):
-    y_pred = model.predict(X)
-    return y_pred
+    model.compile(optimizer=opt,
+                  loss=loss_fn,
+                  metrics=['accuracy'])
+    return model
+
+def convert_example_to_feature(tweet):
+    from transformers import BertTokenizerFast
+    tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+    return tokenizer(tweet, add_special_tokens=True,
+                                    max_length=None,
+                                    pad_to_max_length=True,
+                                    return_attention_mask=True,
+                                    return_token_type_ids=False)
+def softmax(x):
+    return np.exp(x)/sum(np.exp(x))
+
+def run_best_model(X_test):
+    test = []
+    for elem in X_test:
+        tweet = ''
+        for word in elem:
+            tweet+=word+' '
+        test.append(tweet)
+    model = build_model()
+    model.load_weights("./NN_WEIGHTS/full_training_bert_weights_1M.h5")
+    bert_test = convert_example_to_feature(test)
+    input_ids = tf.convert_to_tensor(bert_test.get('input_ids'))
+    attention_mask =tf.convert_to_tensor(bert_test.get('attention_mask'))
+    y_pred = model.predict([input_ids,attention_mask])
+    predictions = [softmax(x) for x in y_pred[0]]
+    output = []
+    for elem in predictions:
+        if elem[0]>elem[1] : x=-1
+        else : x = 1
+        output.append(x)
+    return output
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import argparse
 import os.path
 from models.best_model_implementation import *
+import warnings
+warnings.filterwarnings("ignore")
+
 
 if __name__ == "__main__":
 
@@ -24,15 +27,22 @@ if __name__ == "__main__":
     
     from scripts.helpers_test import *
     from cleaning.data_cleaning import *
+    import tensorflow as tf
     
     test = load_cleaned_data(file_in=file_in, stop_words=False)
-
-    test_df = create_test_dfs(test)
-
-    X_test = test_df['tweets']
-    print(X_test[:3])
-
-    y_pred = run_SVM(X_test)
+    typed = ''
+    while(typed!='SVM' and typed!='BERT'):
+        which_model = input("which model do you want to try ? type SVM or BERT  : ")
+        typed=which_model
+    print('Thank you')     
+    if (which_model == 'SVM'):
+        test_df = create_test_dfs(test)
+        X_test = test_df['tweets']
+        y_pred = run_SVM(X_test)
+    elif(which_model == "BERT"):
+        y_pred = run_best_model(test)
+    else:
+        print('Thank you')
 
     dir_name = os.path.dirname(__file__)
     create_csv_submission(y_pred, dir_name+file_out)
